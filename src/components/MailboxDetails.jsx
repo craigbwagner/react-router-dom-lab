72b346a21297a6228ee89ee0e0831d45562c3fef
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-07-10 13:56:06 -0400

    add ability to find the matching mailbox in the mailboxes array for the MailboxDetails component

--- src/components/MailboxDetails.jsx
+++ src/components/MailboxDetails.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 function MailboxDetails() {
-  const { mailBoxId } = useParams();
+  const { mailboxId } = useParams();
+  const selectedBox = props.mailboxes.find(
+    (mailbox) => mailbox._id === Number(mailboxId)
+  );
+
 	return <div>MailboxDetails</div>;
 }
 
