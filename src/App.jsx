e53b1dc06a088ccfeaf90bb6baa8b7edbf3b0caf
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-07-10 13:52:03 -0400

    add necessary routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
-import Navbar from "./components/Navbar";
+import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+import Navbar from './components/Navbar';
+import MailboxList from './components/MailboxList';
+import MailboxForm from './components/MailboxForm';
+import MailboxDetails from './components/MailboxDetails';
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <h1>Hello world</h1>
-    </>
-  )
+	const [mailboxes, setMailboxes] = useState([]);
+
+	function addBox(formData) {
+		formData._id = mailboxes.length + 1;
+		setMailboxes([...mailboxes, formData]);
+	}
+
+	return (
+		<>
+			<Navbar />
+			<Routes>
+				<Route
+					path='/'
+					element={
+						<main>
+							<h1>Post Office</h1>
+						</main>
+					}
+				></Route>
+				<Route
+					path='mailboxes'
+					element={<MailboxList />}
+				/>
+				<Route
+					path='new-mailbox'
+					element={<MailboxForm />}
+				/>
+				<Route
+					path='mailboxes/:mailboxId'
+					element={<MailboxDetails />}
+				/>
+			</Routes>
+		</>
+	);
 };
 
 export default App;
